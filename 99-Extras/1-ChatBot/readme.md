e719cf04ba39ffe80b9399377dfbf66944e9826c
Erick Dutra <dutra.erick@gmail.com>
2025-06-25 22:28:20 -0300

    Adicionando: Projeto ChatBot

---
+++ 99-Extras/1-ChatBot/readme.md
@@ -0,0 +1,19 @@
+# Categoria Extras - Projeto 01 - ChatBot com Semantic Kernel e LLAMA local (.NET 9)
+
+Este exemplo demonstra um uso básico do Semantic Kernel com LLAMA rodando localmente.
+
+## Pré-requisitos
+
+- Docker com LLAMA via [Ollama](https://ollama.com) instalado:
+  ```bash
+  ollama run phi3
+  ```
+
+- .NET 9 instalado
+
+## Executando
+
+```bash
+dotnet run --project ChatBot
+```
+
---
+++ 99-Extras/1-ChatBot/src/Program.cs
@@ -0,0 +1,48 @@
+﻿using DesbravandoSKComDotNet.Ollama;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using OllamaSharp;
+
+namespace ChatBot
+{
+    // Suppress the diagnostic warning SKEXP0070 as the method is for evaluation purposes only.
+    // This suppression ensures the code can proceed without modification to the method itself.
+    #pragma warning disable SKEXP0070
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var builder = Kernel.CreateBuilder();
+
+            builder.Services.AddScoped<IOllamaApiClient>(_ => new OllamaApiClient("http://localhost:11434"));
+            builder.Services.AddScoped<IChatCompletionService, OllamaChatCompletionService>();
+
+            var kernel = builder.Build();
+            var chatService = kernel.GetRequiredService<IChatCompletionService>();
+            var history = new ChatHistory();
+            
+            history.AddSystemMessage("Você é um assistente completo que me ajudará com minhas dúvidas.");
+
+            while (true)
+            {
+                Console.Write("Você: ");
+                var userMessage = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(userMessage))
+                {
+                    break;
+                }
+
+                history.AddUserMessage(userMessage);
+
+                var response = await chatService.GetChatMessageContentAsync(history);
+
+                Console.WriteLine($"Robô: {response.Content}");
+
+                history.AddMessage(response.Role, response.Content ?? string.Empty);
+            }
+        }
+
+    }
+}
